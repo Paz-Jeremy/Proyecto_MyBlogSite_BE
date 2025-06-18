d8d21ab5926af31bc57830bc2d899e8323b1eca2
Jeremy <jeremyjosecastellanos@gmail.com>
2025-06-17 23:33:16 -0600

    Aumentamos el límite de body para JSON y formularios URL‑encoded, Con esto Express aceptará cuerpos (JSON o URL‑encoded) de hasta 10 MB evitando así el error 413 Payload Too Large.

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+
+// Aumentamos el límite de body para JSON y formularios URL‑encoded
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 //Rutas
 app.use('/api/auth', require('./routes/authRoutes'));
